1b5598a6baf41e346ecabfa58940596a24aa609e
didgjstn585 <com6474@gmail.com>
2023-03-15 23:06:58 +0900

    feat : 회원 등록
    - 회원 등록 및 validation 추가
    - css , js 파일 변경

--- src/main/java/com/springbootjpa/springbootjpa/controller/HomeController.java
+++ src/main/java/com/springbootjpa/springbootjpa/controller/HomeController.java
@@ -1,18 +1,16 @@
 package com.springbootjpa.springbootjpa.controller;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
-@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String home() {
-        log.info("spring boot jpa");
+        log.info("home controller");
         return "home";
     }
 }
--- src/main/java/com/springbootjpa/springbootjpa/controller/MemberController.java
+++ src/main/java/com/springbootjpa/springbootjpa/controller/MemberController.java
@@ -1,4 +1,50 @@
 package com.springbootjpa.springbootjpa.controller;
 
+import com.springbootjpa.springbootjpa.domain.Address;
+import com.springbootjpa.springbootjpa.domain.Member;
+import com.springbootjpa.springbootjpa.domain.MemberDTO;
+import com.springbootjpa.springbootjpa.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    /**
+     * 회원 등록 페이지
+     *
+     * @param model Model attribute
+     * @return 회원 등록 페이지
+     */
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberDTO", new MemberDTO());
+
+        return "member/memberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String save(@Validated MemberDTO memberDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "member/memberForm";
+        }
+        Address address = new Address(memberDTO.getCity(), memberDTO.getStreet(), memberDTO.getZipcode());
+        Member member = new Member();
+        member.setName(memberDTO.getName()); // dto -> entity 변환 방법으로 변경 예정
+        member.setAddress(address);
+
+        memberService.save(member);
+
+        return "redirect:/";
+
+    }
 }
